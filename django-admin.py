19f8b6e0f07ec94e7115c936635fd4a6f1357d76
gabriela denisse hernandez penagos <183422@ids.upchiapas.edu.mx>
2020-06-09 12:31:59 -0500

    first commit

---
+++ django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gabik\OneDrive\Documentos\Cliente Servidor\entorno2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
